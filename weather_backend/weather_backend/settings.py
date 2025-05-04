82d0da772de7bf72c945119f3ba34b19e383131b
SamMochache <sammochache01@gmail.com>
2025-05-04 11:25:11 +0300

    Set up React frontend with Vite, add weather fetching functionality, and configure ESLint

--- weather_backend/weather_backend/settings.py
+++ weather_backend/weather_backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'weather',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite default port
+    ]
 
 ROOT_URLCONF = 'weather_backend.urls'
 
