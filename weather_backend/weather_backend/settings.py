73769e1d230b36c368ad4f09b6fa1344377f7d59
SamMochache <sammochache01@gmail.com>
2025-05-06 16:03:12 +0300

    Load environment variables for SECRET_KEY and DEBUG in settings

--- weather_backend/weather_backend/settings.py
+++ weather_backend/weather_backend/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import json
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,15 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-awwr*%jjyna%e$gy=s0n2!x+d-(yx8p$vt2w&eyn0e!p@fy(7x'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
+# Load from .env
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+ALLOWED_HOSTS=[]
 # Application definition
 
 INSTALLED_APPS = [
